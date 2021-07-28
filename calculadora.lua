a93815409ecf0acb487addb697b5e3e0a42208f5
vitorlbarroso <76413552+vitorlbarroso@users.noreply.github.com>
2021-07-27 22:21:11 -0300

    Add files via upload

---
+++ calculadora.lua
@@ -0,0 +1,26 @@
+-- # Halzz # --
+
+local num1 = 70 -- Primeiro Número
+local num2 = 10 -- Segundo Número
+
+local function calc()
+    local operacao = "adicao" -- adicao/subtracao/divisao/multiplicacao
+
+    -- CÓDIGO CALCULADORA
+
+    if operacao == "adicao" then
+        return num1 + num2
+    elseif operacao == "subtracao" then
+        return num1 - num2
+    elseif operacao == "divisao" then
+        return num1 / num2
+    elseif operacao == "multiplicacao" then
+        return num1 * num2
+    else
+        return "Erro na operação"
+    end
+end
+
+-- CALCULADORA FUNCIONANDO
+
+print("O resultado é: " .. calc()) -- Resultado do Cálculo
